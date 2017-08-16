dd8602e97acc4bc5f3791f2e3770fa56b346e595
Ryan Lewis <ryanharrisonlewis@gmail.com>
2017-08-15 17:42:48 -0700

    missing files

--- azure/handler.js
+++ azure/handler.js
@@ -1,9 +1,9 @@
 require('@std/esm')
 
-const today = require('../today.mjs').default
-const yesterday = require('../yesterday.mjs').default
-const todayGzipped = require('../today.mjs.gz').default
-const yesterdayGzipped = require('../yesterday.mjs.gz').default
+const today = require('./today.mjs').default
+const yesterday = require('./yesterday.mjs').default
+const todayGzipped = require('./today.mjs.gz').default
+const yesterdayGzipped = require('./yesterday.mjs.gz').default
 
 module.exports.hello = function (context) {
   const message = `Today is ${today()} and yesterday was ${yesterday()}
